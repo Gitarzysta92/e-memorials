b21380cc802b3e8bb0cee494ff1697c771c1f4fd
Michał <michal@threesixty.com>
2019-05-30 14:49:52 +0200

    FIX - fix styles for mobile devices

--- app/models/qanda.js
+++ app/models/qanda.js
@@ -10,76 +10,44 @@ module.exports = {
     questions: [
         {
             question: 'W jakim celu została stworzona niniejsza strona?',
-            answer: `Strona internetowa na której Państwo aktualnie się 
-            znajdują powstała w celu upamiętnienia naszych bliskich, którzy 
-            odeszli, jednak nadal są w naszych sercach. Naszym celem jest 
-            pielęgnowanie pamięci o ludziach, którzy znaczyli dla nas bardzo 
-            wiele. Chcemy działać w taki sposób, aby pomóc przechowywać 
-            najcenniejsze co nam po nich pozostało, czyli wspomnienia.`
+            answer: `Strona internetowa, na której Państwo aktualnie się znajdują, powstała w celu upamiętnienia naszych bliskich; tych, którzy już odeszli, lecz nadal są w naszych sercach. Naszym celem jest pielęgnowanie pamięci o ludziach, którzy znaczyli dla nas bardzo wiele. Chcemy działać w taki sposób, aby pomóc przechowywać najcenniejsze, co nam po nich pozostało, czyli wspomnienia.`
         },	
         {
             question: 'Jak mogę stworzyć profil bliskiej mi osoby?',
-            answer: `Stworzenie profilu jest bardzo proste. Należy przejść 
-            jedynie trzy kroki. Zarejestrować wybraną przez siebie osobę 
-            zmarłą, dodać zdjęcia, opisać ją w kilku zdaniach.`
+            answer: `Stworzenie profilu jest bardzo proste. Należy przejść jedynie trzy kroki. 
+            Pierwszym jest zarejestrowanie wybranej przez siebie osoby zmarłej, następnie należy dodać zdjęcia , a potem opisać ją w kilku zdaniach.`
         },
         {
             question: 'Czy założenie profilu jest płatne?',
-            answer: `Założenie profilu wiąże się z kosztami, 
-            które znajdują się w zakładce 'Cennik'. 
-            Tam znajdą Państwo szczegółowe dane odnośnie możliwych pakietów.`
+            answer: `Założenie profilu wiąże się z kosztami, które znajdują się w zakładce 'Cennik'. Tam znajdą też Państwo szczegółowe dane odnośnie możliwych pakietów.`
         },
         {
             question: 'Co to jest QR Code i jak go otrzymam?',
-            answer: `Każdy wysłany QR Code jest unikatowy i przypisany 
-            do konkretnego profilu. Naklejkę z kodem otrzymają Państwo 
-            listownie w ciągu 10 dni roboczych po całkowitym 
-            uzupełnieniu danych profilu oraz uiszczeniu opłaty.`
+            answer: `Każdy wysłany QR Code jest unikatowy i przypisany do konkretnego profilu. Naklejkę z kodem otrzymają Państwo listownie w ciągu 10 dni roboczych po całkowitym uzupełnieniu danych profilu oraz uiszczeniu opłaty.`
         },
         {
             question: 'Co zrobić z QR Codem?',
-            answer: `Otrzymaną naklejkę na której widoczny 
-            będzie specjalny kod należy przylepić do nagrobka. 
-            Po wejściu na naszą stronę oraz zeskanowaniu kodu 
-            z naklejki zostanie wyświetlony profil bliskiej nam osoby. 
-            QR code posiada neutralny kolor, tak aby nie rzucał się w oczy.`
+            answer: `Otrzymaną naklejkę, na której widoczny będzie specjalny kod, należy przylepić do nagrobka. Po wejściu na naszą stronę oraz zeskanowaniu kodu z naklejki, zostanie wyświetlony profil bliskiej nam osoby. QR code posiada neutralny kolor, tak, aby nie rzucał się w oczy.`
         },
         {
             question: 'Dla kogo widoczny będzie profil wybranej przeze mnie osoby?',
-            answer: `Profil z danymi wybranej przez nas osoby będzie widoczny 
-            wyłącznie dla osób, którym wyślemy do niej dostęp.`
+            answer: `Profil z danymi wybranej przez nas osoby będzie widoczny wyłącznie dla osób, którym wyślemy do niego dostęp.`
         },
         {
-           question: 'Jaką liczbę zdjęć powinienem umieścić?',
-           answer: `Każdy z pakietów posiada ograniczoną liczbę zdjęć. 
-           Informacja ta jest również zawarta w zakładce 'Cennik'. 
-           Liczba zdjęć dodanych do profilu jest zależne od nas samych. 
-           Nasza strona ułatwi ich przechowywanie. Mamy świadomość tego, 
-           że jest to bezcenna pamiątka, która przypomina nam o bliskich, 
-           którzy pozostali w naszych sercach. Zakładając profil mamy 
-           pewność, że nie zostaną one zniszczone.` 
+           question: 'Jaką liczbę zdjęć powinienem/ powinnam umieścić?',
+           answer: `Każdy z pakietów posiada ograniczoną liczbę zdjęć. Informacja ta jest również zawarta w zakładce 'Cennik'. Liczba zdjęć dodanych do profilu jest zależna od nas samych. Nasza strona ułatwi ich przechowywanie. Mamy świadomość tego, że zdjęcia są bezcenną pamiątką, która przypomina nam o bliskich pozostałych na zawsze w naszych sercach. Zakładając profil mamy pewność, że nie zostaną one zniszczone przez upływ czasu.` 
         },
         {
             question: 'Jak sprawię, że papierowe zdjęcia trafią na profil?',
-            answer: `Zdjęcia znajdujące się w albumach należy zeskanować, 
-            zapisać na wybranym urządzeniu (np. komputer, laptop), 
-            a następnie załączyć do profilu.`
+            answer: `Zdjęcia znajdujące się w albumach należy zeskanować, zapisać w folderze na wybranym urządzeniu (np. komputer, laptop), a następnie załączyć do profilu.`
         },
         {
-            question: 'Jakie informacje powinienem umieścić opisując bliską osobę?',
-            answer: `Opis, który umieścimy jest także zależny od nas oraz od tego kogo wybraliśmy. 
-            Tworząc stronę chcieliśmy, aby każdy z Państwa opisał najbliższą 
-            sobie osobę w różnych aspektach jego życia. Oczywiście mamy świadomość, 
-            że może to być członek rodziny, przyjaciel, a także znajomy. 
-            Biorąc pod uwagę stopień pokrewieństwa mogą Państwo posiadać 
-            szerokie zaplecze informacji przechodzących z pokolenia na 
-            pokolenie (w przypadku członka rodziny) bądź jedynie historie w 
-            których wspólnie uczestniczyliście (przyjaciel, znajomy).`
+            question: 'Jakie informacje powinienem/powinnam umieścić, opisując bliską osobę?',
+            answer: `Opis, który umieścimy, jest zależny od nas, oraz od tego, kogo wybraliśmy. Tworząc stronę chcieliśmy, aby każdy z Państwa opisał bliską sobie osobę w różnych aspektach jej życia. Oczywiście mamy świadomość, że taka osoba może być członkiem rodziny, przyjacielem, jak również znajomym. Biorąc pod uwagę stopień pokrewieństwa, mogą Państwo posiadać szerokie zaplecze informacji, przechodzące z pokolenia na pokolenie (w przypadku członka rodziny),jak również mogą to być wspomnienia dotyczące historii lub wydarzeń, w których wspólnie Państwo braliście udział (w przypadku przyjaciela, znajomego).`
         },
         {
             question: 'Co mam zrobić w przypadku ewentualnych pytań?',
-            answer: `Chętnie odpowiemy na każde nurtujące Państwa pytanie. 
-            Adres e-mail znajduje się w zakładce 'Kontakt'.`
+            answer: `Chętnie odpowiemy na każde nurtujące Państwa pytanie. Adres e-mail znajduje się w zakładce 'Kontakt'.`
         }
     ]
 }
