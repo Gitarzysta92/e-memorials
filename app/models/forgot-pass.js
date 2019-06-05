b66d0d78c862dc2136a11a1d97a8ae2a218163ab
Michał <michal@threesixty.com>
2019-06-05 18:24:24 +0200

    temp

---
+++ app/models/forgot-pass.js
@@ -0,0 +1,32 @@
+module.exports = {
+    logo: {
+        url: '/images/logo.png',
+        alt: 'Company logo',
+        title: 'e-memorials'
+    },
+    description: {
+        text: `Miejsce gdzie możesz pielnęgnować 
+        pamięć o bliskich, którzy odeszli.`
+    },
+    form: {
+        email: {
+            placeholder: 'Podaj adres e-mail'
+        },
+        button: {
+            text: 'Resetuj hasło',
+            alt: false,
+            url: false,
+            meta: {
+                class: 'solid blue',
+                icon: false
+            }
+        },
+        alert: false
+    },
+    registration: {
+        pre: 'Nie masz konta?',
+        url: '/registration',
+        title: 'Rejestracja',
+        text: 'Zarejestruj się'
+    },
+}
