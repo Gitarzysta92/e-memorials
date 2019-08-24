1a9656362a63c74080bf4e69ad0b4ab9fe8376f6
Michał Łukasiewicz <michal@threesixty.com>
2019-08-24 19:34:12 +0200

    Add menu active element checker

--- app/services/pages-composer/pages.js
+++ app/services/pages-composer/pages.js
@@ -29,23 +29,49 @@ module.exports = function({modelComposer, database, ...rest}) {
 			content: JSON.parse(page.content),
 		})))
 		
-		const menu = pagesData.reduce((acc, page) => {
-				if (!Array.isArray(page.meta.menu)) return acc;	
-				const isExist = page.meta.menu.find(access => access === userAccess);
-				return isExist ? [...acc, page.meta] : acc
-		}, [])
 		const page = pagesData.find(page => page.meta.path === url);
-
 		if (!page) return;
-		
+
+		const menu = getMenuItems(pagesData, userAccess);
 		const pageData = modelComposer.preparePageModel({ menu: menu, main: page.content });
+
+		pageData.data.menu.navItems = setCurrentMenuItem(pageData.data.menu.navItems, url); 
+		
+		console.log(pageData);
 		return pageData;
 	}
+
+
+	function getMenuItems(pagesData, userAccess) {
+		const menuItems = pagesData.reduce((acc, page) => {
+			if (!Array.isArray(page.meta.menu)) return acc;	
+			const isExist = page.meta.menu.find(access => access === userAccess);
+			return isExist ? [...acc, page.meta] : acc
+		}, [])
+
+		return menuItems.sort((curr, next) => curr.menuPosition - next.menuPosition);
+	}
+
+
+	function setCurrentMenuItem(menu, url) {
+		const firstLevel = new RegExp('^' + url + '$', 'g');
+		
+		return menu.map(item => {
+			console.log(item)
+			firstLevel.test(item.path) && (item.class = 'active');
+
+			return item;
+		});
+	}
+
 
 	return {
 		getPageData
 	}
 }
+
+
+
 
 
 
